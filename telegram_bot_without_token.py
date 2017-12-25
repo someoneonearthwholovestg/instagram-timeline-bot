cfab98b9889804fe78fa73bb715c54434ef2972a
tycheyoung <retbird13@gmail.com>
2017-12-25 00:28:37 +0000

    Initial commit

---
+++ telegram_bot_without_token.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import url_request
+import csv
+import os,sys
+import time
+
+filename = 'subscribe.csv'
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+def help(bot, update):
+    update.message.reply_text('What you can order: \n /add, /remove, /list, /on, /initiate')
+
+def subscription_list(bot, update):
+    """Echo the subscription list."""
+    update.message.reply_text("\n".join(url_request.print_subscribe_list()))
+
+def initiate(bot, update):
+    """Flush all subscription list."""
+    url_request.initiate_list()
+    update.message.reply_text('Terminated all subscription list!')
+
+def add_subscription(bot, update, args):
+    """Add Instagram ID to subscription list."""
+    chat_id = update.message.chat_id
+    try:
+        results = url_request.add_subscribe(args[0])
+        if (results == -2):
+          update.message.reply_text('Successfully added to subscription list!')
+        elif (results == -3):
+          update.message.reply_text('Already exists in subscription list!')
+        elif (results == -4):
+          update.message.reply_text("Page doesn't exist or private account")
+          
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <Instagram ID>')
+
+def unsubscribe(bot, update, args):
+    """Add Instagram ID to subscription list."""
+    chat_id = update.message.chat_id
+    try:
+        results = url_request.unsubscribe(args[0])
+        if (results == -3):
+          update.message.reply_text('There is nothing to unsubscribe')
+        elif (results == -2):
+          update.message.reply_text('Successfully removed from subscription list!')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /remove <Instagram ID>')
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+def callback_feedupdater(bot, job):
+    with open(filename, 'rt') as infile, open('outfile.csv', 'a') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        try:
+            for row in reader:
+                profile = url_request.profile_address(row[0])
+                # print(profile)
+                url_temp = url_request.find_latest(profile)
+                if(row[1]!=url_temp):
+                    row[1] = url_temp
+                    bot.send_message(chat_id=job.context, text=row[1])
+                try:
+                    writer.writerow(row)
+                except IndexError:
+                    bot.send_message(chat_id=job.context, text=row[0]+'changed to private account or not existing')
+#                time.sleep(20)
+            os.remove(filename)
+            os.rename("outfile.csv", filename)
+        except csv.Error as e:
+            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+            bot.send_message(chat_id=job.context, text='CSV File Error!')
+
+def callback_timer(bot, update, job_queue):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='Notification ON!')
+
+    job_queue.run_repeating(callback_feedupdater, interval=60, context=update.message.chat_id)
+
+def main():
+    """Start the bot."""
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("YOUR_TOKEN_HERE")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Cron_updater
+    feed_handler = CommandHandler("on", callback_timer, pass_job_queue=True)
+    dp.add_handler(feed_handler)
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("hello", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("initiate", initiate))
+    dp.add_handler(CommandHandler("list", subscription_list))
+    dp.add_handler(CommandHandler("add", add_subscription, pass_args=True))
+    dp.add_handler(CommandHandler("remove", unsubscribe, pass_args=True))
+
+    # on noncommand i.e message - echo the message on Telegram
+    # dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+    logger.info('Started!')
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ url_request.py
@@ -0,0 +1,94 @@
+import csv, os
+import webkit_server
+filename = 'subscribe.csv'
+
+def profile_address(insta_id):
+  """returns instagram profile address"""
+  insta_url = "https://instagram.com/"+insta_id
+  return insta_url
+
+def find_latest(insta_url):
+  """find latest post address from instagram profile URL"""
+  import dryscrape
+  from bs4 import BeautifulSoup
+  dryscrape.start_xvfb()
+  server = webkit_server.Server()
+  server_conn = webkit_server.ServerConnection(server=server)
+  driver = dryscrape.driver.webkit.Driver(connection=server_conn)
+  sess = dryscrape.Session(driver=driver)
+  sess.set_attribute('auto_load_images', False)
+  sess.visit(insta_url)
+  response = sess.body()
+  sess.reset()
+  soup = BeautifulSoup(response, "lxml")
+  latest_post = soup.select('div._cmdpi a[href]')
+  #print(latest_post)
+  try:
+    ying = latest_post[0].get('href')
+  except IndexError:
+    return 'NULL'
+  latest_post_url = "https://instagram.com"+ying
+  server.kill()
+  return latest_post_url
+
+def duplicate_check(insta_id):
+  import sys
+  with open(filename, newline='') as f:
+      reader = csv.reader(f)
+      try:
+          for idx,row in enumerate(reader):
+              if(row[0]==insta_id):
+                return idx
+          return -1
+      except csv.Error as e:
+          sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+
+def add_subscribe(insta_id):
+  with open(filename, 'a') as newFile:
+    if(duplicate_check(insta_id)==-1):
+      newFileWriter = csv.writer(newFile)
+      try:
+        latest_url = find_latest(profile_address(insta_id))
+        newFileWriter.writerow([insta_id, latest_url])
+        return -2
+      except IndexError:
+        return -4
+        # print("page doesn't exist or private account")
+    else:
+      # print("duplicated subscription request")
+      return -3
+
+def unsubscribe(insta_id):
+  row_num = duplicate_check(insta_id)
+  ROWS_TO_DELETE = {row_num}
+  if(row_num == -1):
+    # print("there is nothing to unsubscribe")
+    return -3
+  else:
+    with open(filename, 'rt') as infile, open('outfile.csv', 'wt') as outfile:
+      outfile.writelines(row for row_num, row in enumerate(infile, 0)
+                          if row_num not in ROWS_TO_DELETE)
+    os.remove(filename)
+    os.rename("outfile.csv", filename)
+    return -2
+
+def print_subscribe_list():
+  ans = []
+  with open(filename, newline='') as f:
+      reader = csv.reader(f)
+      try:
+          for row in reader:
+              ans.append(row[0])
+          return ans
+      except csv.Error as e:
+          sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+
+def initiate_list():
+  """remove csv file"""
+  os.remove(filename)
+
+# if __name__ == "__main__":
+#   import argparse
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument("--id", help="Instagram ID")
+#   args = parser.parse_args()
